c8c322f85e1992cdc26bf822654d12e9e3e8b7ea
lance.huang <lance.huang@deltaww.com>
2020-08-14 02:03:46 +0800

    remove_last_from

---
+++ datastructure/src/main/java/leecode/arraylist/RemoveLastNode.java
@@ -0,0 +1,55 @@
+package leecode.arraylist;
+
+public class RemoveLastNode {
+
+  public static void main(String[] args) {
+
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode node4 = new ListNode(4);
+    ListNode node5 = new ListNode(5);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+
+    printListNode(node1);
+
+    Solution solution = new Solution();
+    ListNode dummy = new ListNode(520);
+    dummy.next = node1;
+
+    ListNode listNode = solution.removeNthFromEnd(dummy, 2);
+    printListNode(listNode);
+
+  }
+
+  private static void printListNode(ListNode listNode) {
+    System.out.println("listNode = " + listNode.val);
+    if (listNode.next != null) {
+      printListNode(listNode.next);
+    }
+  }
+
+  private static class Solution {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+      ListNode fast = head;
+      ListNode slow = head;
+
+      for (int i = 0; i < n; i++) {
+        fast = fast.next;
+      }
+
+      while (fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+      }
+
+      slow.next = slow.next.next;
+
+      return head;
+    }
+  }
+}
