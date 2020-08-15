78b807a4b7f5741f44ba2efc73f7b8ad7bbd41e7
lance.huang <lance.huang@deltaww.com>
2020-08-15 09:55:27 +0800

    #Single Number

---
+++ src/test/java/leecode/arraylist/MaximumDistanceInArraysTest.java
@@ -0,0 +1,18 @@
+package leecode.arraylist;
+
+import junit.framework.TestCase;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumDistanceInArraysTest {
+
+  int[][] array1 = {{1, 2, 3}, {4, 5}, {1, 2, 3}};
+
+  @Test
+  public void test() {
+    MaximumDistanceInArrays maximumDistanceInArrays = new MaximumDistanceInArrays();
+    int result = maximumDistanceInArrays.maxDistance(array1);
+    Assert.assertThat(result, Is.is(4));
+  }
+}
