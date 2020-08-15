c8d7f32d9cc0de68660343520460b407ee01780f
lance.huang <lance.huang@deltaww.com>
2020-08-15 08:58:56 +0800

    #Single Number

--- src/main/java/leecode/arraylist/SingleNumber.java
+++ src/main/java/leecode/arraylist/SingleNumber.java
@@ -4,55 +4,49 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class SingleNumber {
+
+  static Function<Integer[], Integer> bitmapSolution =
+      nums -> {
+        int result = 0;
+        for (int value : nums) {
+          result ^= value;
+        }
+        return result;
+      };
+
+  static Function<Integer[], Integer> mapSolution =
+      nums -> {
+        Map<Integer, Integer> cache = new HashMap<>();
+
+        for (Integer num : nums) {
+          cache.merge(num, 1, Integer::sum);
+        }
+
+        for (Entry<Integer, Integer> entry : cache.entrySet()) {
+          if (entry.getValue() == 1) {
+            return entry.getKey();
+          }
+        }
+        return 0;
+      };
 
   public static void main(String[] args) {
 
     int[] INTEGER_ARRAY1 = {2, 2, 5, 3, 3, 4, 4};
     int[] INTEGER_ARRAY2 = {4, 1, 2, 1, 2};
 
-    Solution solution = new Solution2();
-    int i = solution.singleNumber(INTEGER_ARRAY1);
-    System.out.println("i = " + i);
-  }
-}
+    Integer result =
+        bitmapSolution.apply(IntStream.of(INTEGER_ARRAY1).boxed().toArray(Integer[]::new));
 
-abstract class Solution {
+    System.out.println("result = " + result);
 
-  abstract int singleNumber(int[] nums);
-}
+    Integer mapResult =
+        mapSolution.apply(IntStream.of(INTEGER_ARRAY2).boxed().toArray(Integer[]::new));
 
-class Solution1 extends Solution {
-
-  @Override
-  int singleNumber(int[] nums) {
-    {
-      int result = 0;
-      for (int value : nums) {
-        result ^= value;
-      }
-      return result;
-    }
-  }
-}
-
-class Solution2 extends Solution {
-
-  @Override
-  int singleNumber(int[] nums) {
-    Map<Integer, Integer> cache = new HashMap<>();
-
-    for (int i = 0; i < nums.length; i++) {
-      cache.merge(nums[i], 1, Integer::sum);
-    }
-    Iterator<Entry<Integer, Integer>> iterator = cache.entrySet().iterator();
-    while (iterator.hasNext()) {
-      Entry<Integer, Integer> entry = iterator.next();
-      if (entry.getValue() == 1) {
-        return entry.getKey();
-      }
-    }
-    return 0;
+    System.out.println("mapResult = " + mapResult);
   }
 }
