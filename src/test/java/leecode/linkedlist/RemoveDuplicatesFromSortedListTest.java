06586f5bc7fe5656b21453e961c73547d5f578f3
lance.huang <lance.huang@deltaww.com>
2020-08-22 12:15:31 +0800

    #Palindrome Linked List

--- src/test/java/leecode/linkedlist/RemoveDuplicatesFromSortedListTest.java
+++ src/test/java/leecode/linkedlist/RemoveDuplicatesFromSortedListTest.java
@@ -1,6 +1,9 @@
 package leecode.linkedlist;
 
+import static org.junit.Assert.assertThat;
+
 import leecode.common.ListNode;
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,6 +20,6 @@ public class RemoveDuplicatesFromSortedListTest {
     ListNode listNode =
         solution.deleteDuplicates(ListNode.buildNode(new int[] {1, 1, 2, 2, 2, 3, 3}));
     ListNode listNode1 = solution.deleteDuplicates(listNode);
-    System.out.println("listNode1 = " + listNode1.toArrayString());
+    assertThat(listNode1.toArrayString(), Is.is("[1,2,3]"));
   }
 }
