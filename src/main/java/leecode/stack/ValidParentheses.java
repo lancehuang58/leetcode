2b1e3e32067c6c693b83da6f5b965e34c57eb61c
lance.huang <lance.huang@deltaww.com>
2020-08-26 13:08:59 +0800

    housekeeping

--- src/main/java/leecode/stack/ValidParentheses.java
+++ src/main/java/leecode/stack/ValidParentheses.java
@@ -12,26 +12,22 @@ public class ValidParentheses {
         case '(':
         case '[':
         case '{':
-//          System.out.println("input c = " + c);
           characterStack.push(c);
           break;
         case ')':
           Character pop = characterStack.pop();
-//          System.out.println("pop = " + pop);
           if (pop != '(') {
             return false;
           }
           break;
         case ']':
           Character pop2 = characterStack.pop();
-          System.out.println("pop2 = " + pop2);
           if (pop2 != '[') {
             return false;
           }
           break;
         case '}':
           Character pop3 = characterStack.pop();
-          System.out.println("pop3 = " + pop3);
           if (pop3 != '{') {
             return false;
           }
