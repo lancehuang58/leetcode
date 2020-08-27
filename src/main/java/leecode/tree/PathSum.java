fcf6eab8df77929bbf813c171492d3834200f35a
lance.huang <lance.huang@deltaww.com>
2020-08-27 11:44:04 +0800

    PathSum

--- src/main/java/leecode/tree/PathSum.java
+++ src/main/java/leecode/tree/PathSum.java
@@ -1,5 +1,8 @@
 package leecode.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import leecode.common.TreeNode;
 
 public class PathSum {
@@ -9,6 +12,27 @@ public class PathSum {
       return false;
     }
 
-    return false;
+    List<Integer> sumPath = new ArrayList<>();
+    Boolean result = new Boolean(false);
+    traverse(sumPath, root, sum, result);
+    System.out.println("result = " + result);
+    return result;
+  }
+
+  private void traverse(List<Integer> sumPath, TreeNode root, int sum, Boolean result) {
+    if (root != null) {
+      sumPath.add(root.val);
+      if (root.right == null && root.left == null) {
+        int r = sumPath.stream().collect(Collectors.summingInt(Integer::intValue)).intValue();
+        result = (r == sum);
+      }
+
+      if (root.right != null) {
+        traverse(sumPath, root.right, sum, result);
+      }
+      if (root.left != null) {
+        traverse(sumPath, root.left, sum, result);
+      }
+    }
   }
 }
--- src/test/java/leecode/tree/PathSumTest.java
+++ src/test/java/leecode/tree/PathSumTest.java
@@ -2,6 +2,8 @@ package leecode.tree;
 
 import static org.junit.Assert.*;
 
+import java.util.TreeMap;
+import leecode.common.TreeNode;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,4 +21,32 @@ public class PathSumTest {
     boolean b = solution.hasPathSum(null, 0);
     assertFalse(b);
   }
+
+  @Test
+  public void given_single_node_then_return_true() {
+    boolean a = solution.hasPathSum(TreeNode.of(1), 1);
+    assertTrue(a);
+  }
+
+  @Test
+  public void given_two_nodes_then_return_true() {
+    TreeNode root = new TreeNode(1, TreeNode.of(2), null);
+    boolean r = solution.hasPathSum(root, 3);
+    assertTrue(r);
+  }
+
+  @Test
+  public void given_3_nodes_then_reture_true() {
+    TreeNode node2 = new TreeNode(2, TreeNode.of(3), null);
+    TreeNode root = new TreeNode(1, node2, null);
+    boolean r = solution.hasPathSum(root, 6);
+    assertTrue(r);
+  }
+
+  @Test
+  public void given_single_node_then_return_false() {
+    boolean r = solution.hasPathSum(TreeNode.of(1), 10);
+    assertFalse(r);
+  }
+
 }
