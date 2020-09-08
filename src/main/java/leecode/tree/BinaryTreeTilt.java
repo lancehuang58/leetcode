819a2151dfa621e5e14dad93a5c5c602caa0fd69
lance.huang <lance.huang@deltaww.com>
2020-09-08 12:33:44 +0800

    #BinaryTreeTilt

--- src/main/java/leecode/tree/BinaryTreeTilt.java
+++ src/main/java/leecode/tree/BinaryTreeTilt.java
@@ -18,16 +18,9 @@ public class BinaryTreeTilt {
     if (node == null) {
       return 0;
     }
-
-    System.out.println("node = " + node.val);
-
     int left = helper(node.left);
-    System.out.println("left = " + left);
     int right = helper(node.right);
-    System.out.println("right = " + right);
-
     diff += abs(left - right);
-
     return node.val + left + right;
   }
 }
