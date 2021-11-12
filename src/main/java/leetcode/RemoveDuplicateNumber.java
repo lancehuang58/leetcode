0f5cc9fa3422d103eed88b30a4ba869400dfd1d6
Lance Huang <LanceHuang79@gmail.com>
2021-11-12 19:49:05 +0800

    update

--- src/main/java/leetcode/RemoveDuplicateNumber.java
+++ src/main/java/leetcode/RemoveDuplicateNumber.java
@@ -1,24 +1,22 @@
 package leetcode;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class RemoveDuplicateNumber {
 
-  public int removeDuplicates(int[] numbers) {
+    public int removeDuplicates(int[] numbers) {
 
-    if (null == numbers || numbers.length == 0) {
-      return 0;
-    }
+        if (null == numbers || numbers.length == 0) {
+            return 0;
+        }
 
-    List<Integer> list = new LinkedList<>();
-    for (int i = 0; i < numbers.length; i++) {
-      int k = i - 1;
-      if (k < 0 || numbers[k] != numbers[i]) {
-        list.add(numbers[i]);
-      }
+        int i = 0;
+        for (int j = 1; j < numbers.length; j++) {
+
+            if (numbers[j] != numbers[i]) {
+                i++;
+                numbers[i] = numbers[j];
+            }
+        }
+        return i+1;
     }
-    return list.size();
-  }
 
 }
