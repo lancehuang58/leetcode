954ec920380b750917ab927bdaa95836ec381919
Lance.HP <lancehuang79@gmail.com>
2021-11-23 10:09:47 +0800

    test

--- src/main/java/leetcode/ThreeSum.java
+++ src/main/java/leetcode/ThreeSum.java
@@ -6,7 +6,6 @@ public class ThreeSum {
 
     public static Set<List<Integer>> findZeroSum(int[] nums) {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
         Set<List<Integer>> result = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
