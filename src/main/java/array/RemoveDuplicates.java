2dd7f4b811efa31b9a2a573e578630f9dc82b07e
Lance Huang <lance.huang@deltaww.com>
2022-02-11 14:14:10 +0800

    remove duplicate numbers in array

--- src/main/java/array/RemoveDuplicates.java
+++ src/main/java/array/RemoveDuplicates.java
@@ -1,7 +1,20 @@
 package array;
 
 public class RemoveDuplicates {
-  public int execute() {
-    return 0;
+
+  public int execute(int[] numbers) {
+
+    if (numbers.length == 0)
+      return 0;
+
+    int i = 0;
+
+    for (int j = 0; j < numbers.length; j++) {
+      if (numbers[j] != numbers[i]) {
+        i++;
+        numbers[i] = numbers[j];
+      }
+    }
+    return i + 1;
   }
 }
---
+++ src/test/java/array/RemoveDuplicateNumbersTest.java
@@ -0,0 +1,28 @@
+package array;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RemoveDuplicateNumbersTest {
+    RemoveDuplicates processor = new RemoveDuplicates();
+
+    @Test
+    void give_empty_array_when_process_then_return_0() {
+        Assertions.assertThat(processor.execute(new int[] {})).isZero();
+    }
+
+    @Test
+    void given_array_when_find_max_number_location_then_return_2() {
+        Assertions.assertThat(processor.execute(new int[] { 1, 2 })).isEqualTo(2);
+    }
+
+    @Test
+    void givne_array_when_find_max_location_then_return_2() {
+        Assertions.assertThat(processor.execute(new int[] { 1, 2, 2 })).isEqualTo(2);
+    }
+
+    @Test
+    void given_array_6_when_find_max_location_the_return_3() {
+        Assertions.assertThat(processor.execute(new int[] { 1, 1, 2, 2, 3, 3 })).isEqualTo(3);
+    }
+}
