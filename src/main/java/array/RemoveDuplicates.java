410c5af06a21c65698345b214021d1cc829d6be2
Lance Huang <lance.huang@deltaww.com>
2022-02-11 14:19:35 +0800

    remove duplicate numbers

--- src/main/java/array/RemoveDuplicates.java
+++ src/main/java/array/RemoveDuplicates.java
@@ -4,17 +4,6 @@ public class RemoveDuplicates {
 
   public int execute(int[] numbers) {
 
-    if (numbers.length == 0)
-      return 0;
-
-    int i = 0;
-
-    for (int j = 0; j < numbers.length; j++) {
-      if (numbers[j] != numbers[i]) {
-        i++;
-        numbers[i] = numbers[j];
-      }
-    }
-    return i + 1;
+    return 0;
   }
 }
