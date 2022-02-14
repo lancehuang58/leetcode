fbbff67d6b3c1877d4f942951ca930a3e1068eb3
Lance Huang <lance.huang@deltaww.com>
2022-02-14 10:35:33 +0800

    array intersection

---
+++ src/test/java/array/ArrayIntersectionTest.java
@@ -0,0 +1,28 @@
+package array;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArrayIntersectionTest {
+
+    ArrayIntersection processor = new ArrayIntersection();
+
+    @Test
+    void test_array_intersection() {
+        int[] result = processor.getIntersction(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 });
+        Assertions.assertThat(result).containsExactlyInAnyOrder(2, 2);
+    }
+
+    @Test
+    void test_array_intersction_2() {
+        int[] result = processor.getIntersction(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 });
+        Assertions.assertThat(result).containsExactlyInAnyOrder(9, 4);
+    }
+
+    @Test
+    void test_array_intersction_3() {
+        int[] result = processor.getIntersction(new int[] { 3, 1, 2 }, new int[] { 1, 1 });
+        Assertions.assertThat(result).containsExactlyInAnyOrder(1);
+    }
+
+}
--- src/test/java/array/RotateArrayTest.java
+++ src/test/java/array/RotateArrayTest.java
@@ -3,17 +3,13 @@ package array;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 class RotateArrayTest {
-
 
   @Test
   void test() {
     RotateArray processor = new RotateArray();
-    int[] nums = {1, 2, 3, 4, 5, 6, 7};
+    int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
     processor.rotate(nums, 3);
-    System.out.println(Arrays.toString(nums));
     Assertions.assertThat(nums).containsExactly(5, 6, 7, 1, 2, 3, 4);
   }
 }
