970217a9bcb65cb67c66e973b87e3445950c4d8b
Lance Huang <lance.huang@deltaww.com>
2022-02-15 17:30:55 +0800

    valida anagram

---
+++ src/test/java/string/ValidAnagramTest.java
@@ -0,0 +1,29 @@
+package string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidAnagramTest {
+
+  ValidAnagram processor = new ValidAnagram();
+
+  @Test
+  void test_validation() {
+    boolean r = processor.isAnagram("abc", "cba");
+    Assertions.assertTrue(r);
+  }
+
+  @Test
+  void test_validation2() {
+    boolean r = processor.isAnagram("anagram", "nagaram");
+    Assertions.assertTrue(r);
+  }
+
+  @Test
+  void test_validation3() {
+    boolean r = processor.isAnagram("rat", "car");
+    Assertions.assertFalse(r);
+  }
+}
