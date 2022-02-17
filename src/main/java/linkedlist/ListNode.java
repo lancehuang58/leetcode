177143f1720edc528425a31ac3a330a00a71929b
Lance Huang <lance.huang@deltaww.com>
2022-02-17 15:42:20 +0800

    delete given node

--- src/main/java/linkedlist/ListNode.java
+++ src/main/java/linkedlist/ListNode.java
@@ -7,4 +7,51 @@ public class ListNode {
   public ListNode(int val) {
     this.val = val;
   }
+
+  public int getVal() {
+    return val;
+  }
+
+  public ListNode setNext(ListNode next) {
+    this.next = next;
+    return this;
+  }
+
+  public ListNode getNext() {
+    return next;
+  }
+
+  public boolean hasNext() {
+    return next != null;
+  }
+
+  public static ListNode of(Integer... values) {
+    ListNode head = null;
+    ListNode current = null;
+    int i = 0;
+    while (i < values.length) {
+      if (head == null) {
+        current = new ListNode(values[i]);
+        head = current;
+      } else {
+        ListNode listNode = new ListNode(values[i]);
+        current.setNext(listNode);
+        current = listNode;
+      }
+      i++;
+    }
+    return head;
+  }
+
+  public void print() {
+    System.out.print(val);
+  }
+
+  public void printAll() {
+    print();
+    if (this.hasNext()) {
+      System.out.print(",");
+      next.printAll();
+    }
+  }
 }
---
+++ src/test/java/linkedlist/DeleteNodeTest.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteNodeTest {
+  DeleteNode processor = new DeleteNode();
+  @Test
+  public void test_delete_node() {
+    ListNode head = ListNode.of(4, 5, 1, 9);
+    head.printAll();
+    System.out.println();
+    ListNode node1 = head.next.next;
+    processor.deleteNode(node1);
+    head.printAll();
+  }
+}
