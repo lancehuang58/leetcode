0f5f4cf77b6b0fe613320c919a0a504d3ef1aff2
Lance Huang <lance.huang@deltaww.com>
2022-02-21 11:31:00 +0800

    reverse link list

---
+++ src/test/java/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+package linkedlist;
+
+public class ReverseLinkedList {
+
+  public ListNode reverseList(ListNode head) {
+    //declare nodes previous/current/next
+    ListNode current = head, previous = null, next = null;
+    while (current != null) {
+      //1.get next cached
+      next = current.next;
+      //2 current node.next = previous
+      current.next = previous;
+      //previous node as current node
+      previous = current;
+      //move current to next node
+      current = next;
+    }
+    //set the head node as last node
+    head = previous;
+    return head;
+  }
+}
---
+++ src/test/java/linkedlist/ReverseLinkedListTest.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+class ReverseLinkedListTest {
+
+  ReverseLinkedList processor = new ReverseLinkedList();
+
+  @Test
+  void test_revers_list() {
+    ListNode node = ListNode.of(1, 2, 3, 4, 5);
+    ListNode listNode = processor.reverseList(node);
+    listNode.printAll();
+  }
+}
