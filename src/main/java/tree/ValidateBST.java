ab9c28a405c0a83eb18da89953dfab2667bc34ea
Lance Huang <lance.huang@deltaww.com>
2022-02-24 11:21:31 +0800

    validate binary search tree.

--- src/main/java/tree/ValidateBST.java
+++ src/main/java/tree/ValidateBST.java
@@ -1,20 +1,16 @@
 package tree;
-//solution: traverse all node from smallest to largest,
-//if there is any node smaller than or equal current node value, return false;
+// solution: traverse all node from smallest to largest,
+// if there is any node smaller than or equal current node value, return false;
 public class ValidateBST {
   TreeNode last;
 
   public boolean isValidBST(TreeNode root) {
-    if (root == null) return true;
-
-    if (!isValidBST(root.left)) {
-      return false;
+    if (root == null) {
+      return true;
     }
-
+    if (!isValidBST(root.left)) return false;
     if (last != null && root.val <= last.val) return false;
-
     last = root;
-
     return isValidBST(root.right);
   }
 }
