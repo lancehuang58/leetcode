485ec1461b851eda4f4e2265d805a8acab3cca5c
Lance Huang <lance.huang@deltaww.com>
2022-02-24 09:35:41 +0800

    figure out the logic for all process

---
+++ src/main/java/array/RemoveDuplicateNumbersFromSortedArray.java
@@ -0,0 +1,21 @@
+package array;
+
+public class RemoveDuplicateNumbersFromSortedArray {
+
+    public int execute(int[] numbers) {
+
+        if (numbers.length == 0)
+            return 0;
+
+        int i = 0;
+
+        for (int j = 1; j < numbers.length; j++) {
+            if (numbers[j] != numbers[i]) {
+                i++;
+                numbers[i] = numbers[j];
+            }
+        }
+        return i + 1;
+    }
+
+}
