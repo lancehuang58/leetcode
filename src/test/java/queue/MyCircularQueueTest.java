c1cf8e464f456af5ba87cc99136642142b12fa65
Lance Huang <lance.huang@deltaww.com>
2022-03-10 16:40:40 +0800

    test

--- src/test/java/queue/MyCircularQueueTest.java
+++ src/test/java/queue/MyCircularQueueTest.java
@@ -2,22 +2,21 @@ package queue;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;;
 
 class MyCircularQueueTest {
 
   @Test
   void test() {
     MyCircularQueue queue = new MyCircularQueue(3);
-    queue.enQueue(1);
-//    queue.enQueue(2);//
-//    queue.enQueue(3);
-//    queue.enQueue(4);
-    int rear = queue.rear();
-    System.out.println("rear -> "+rear);
-//    queue.isFull();
-//    queue.deQueue();
-//    queue.enQueue(4);
-//    queue.rear();
+    assertThat(queue.enQueue(1)).isTrue();
+    assertThat(queue.enQueue(2)).isTrue();
+    assertThat(queue.enQueue(3)).isTrue();
+    assertThat(queue.enQueue(4)).isFalse();
+    assertThat(queue.Rear()).isEqualTo(3);
+    assertThat(queue.isFull()).isTrue();
+    assertThat(queue.deQueue()).isTrue();
+    assertThat(queue.enQueue(4)).isTrue();
+    assertThat(queue.Rear()).isEqualTo(4);
   }
 }
