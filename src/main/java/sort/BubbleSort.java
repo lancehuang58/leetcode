40f266693a51da78832571c5b99c7b56dacab9de
lance.huang <lancehuang79@gmail.com>
2022-03-28 15:56:58 +0800

    bubble sort rewrite

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -3,22 +3,17 @@ package sort;
 public class BubbleSort {
 
     public void execute(int[] nums) {
-        int temp;
-        for (int i = nums.length - 1; i > 0; i--) {
-            boolean swap = false;
-            for (int j = 0; j < i; j++) {
-                int k = j + 1;
-                if (nums[k] < nums[j]) {
-                    temp = nums[j];
-                    nums[j] = nums[k];
-                    nums[k] = temp;
-                    swap = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1 -i; j++) {
+
+                if (nums[j] > nums[j + 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
                 }
             }
-            //if flag is false , the array has been sorted.
-            if (!swap) {
-                break;
-            }
+            System.out.println();
         }
     }
 }
--- src/test/java/sort/BubbleSortTest.java
+++ src/test/java/sort/BubbleSortTest.java
@@ -3,17 +3,16 @@ package sort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class BubbleSortTest {
 
   BubbleSort sort = new BubbleSort();
 
   @Test
   void test_sort() {
-
     int[] nums = new int[] {6, 5, 9, 7, 2, 8};
-
     sort.execute(nums);
-
     Assertions.assertThat(nums).containsExactly(2, 5, 6, 7, 8, 9);
   }
 
