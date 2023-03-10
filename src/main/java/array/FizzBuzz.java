a01cb45f20385c23c2258b7499de63c99a00c2dd
Lance Huang <lance.huang@deltaww.com>
2023-03-10 13:30:22 +0800

    fizzbuzz/test

---
+++ src/main/java/array/FizzBuzz.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    /**
+     *
+     */
+    public static class Solution {
+
+        public List<String> fizzBuzz(int n) {
+            List<String> result = new ArrayList<>();
+            for (int i = 1; i < n + 1; i++) {
+                if (i % 15 == 0) {
+                    result.add("FizzBuzz");
+                } else if (i % 5 == 0) {
+                    result.add("Buzz");
+                } else if (i % 3 == 0) {
+                    result.add("Fizz");
+                } else {
+                    result.add(String.valueOf(i));
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/test/java/array/FizzBuzzTest.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    FizzBuzz.Solution game = new FizzBuzz.Solution();
+
+    @Test
+    void test_fizz() {
+        List<String> result = game.fizzBuzz(3);
+        Assertions.assertThat(result).containsExactly("1", "2", "Fizz");
+    }
+
+    @Test
+    void test_buzz() {
+        List<String> result = game.fizzBuzz(5);
+        Assertions.assertThat(result).containsExactly("1", "2", "Fizz", "4", "Buzz");
+    }
+
+    @Test
+    void test_fizz_buzz() {
+        List<String> result = game.fizzBuzz(15);
+        Assertions.assertThat(result)
+                .containsExactly("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13",
+                        "14", "FizzBuzz");
+    }
+
+}
