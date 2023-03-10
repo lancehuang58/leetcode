f0725b1e77d9c6ed52e0eb39196f2c889dd729f3
Lance Huang <lance.huang@deltaww.com>
2023-03-10 14:24:56 +0800

    middle of linked list.

---
+++ src/main/java/linkedlist/MiddleOfLinkedList.java
@@ -0,0 +1,9 @@
+package linkedlist;
+
+public class MiddleOfLinkedList {
+    public static class Solution {
+        public ListNode middleNode(ListNode head) {
+            return null;
+        }
+    }
+}
---
+++ src/test/java/linkedlist/MiddleOfLinkedListTest.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+class MiddleOfLinkedListTest {
+
+    final MiddleOfLinkedList.Solution solution = new MiddleOfLinkedList.Solution();
+
+    @Test
+    void test_solution() {
+
+
+    }
+}
