5ffcf13e6f9efa4eb75a2d619f25750b54045f76
Lance Huang <lance.huang@deltaww.com>
2023-03-10 13:45:28 +0800

    fizzbuzz/test

--- src/main/java/array/FizzBuzz.java
+++ src/main/java/array/FizzBuzz.java
@@ -2,22 +2,28 @@ package array;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FizzBuzz {
+    static Predicate<Integer> divisibleBy3 = i -> i % 3 == 0;
+    static Predicate<Integer> divisibleBy5 = i -> i % 5 == 0;
+
+
     /**
-     *
+     * Time Complexity :　O(n)
+     * Space Complexity: O(1)
      */
     public static class Solution {
 
         public List<String> fizzBuzz(int n) {
             List<String> result = new ArrayList<>();
             for (int i = 1; i < n + 1; i++) {
-                if (i % 15 == 0) {
+                if (divisibleBy3.test(i) && divisibleBy5.test(i)) {
                     result.add("FizzBuzz");
-                } else if (i % 5 == 0) {
-                    result.add("Buzz");
-                } else if (i % 3 == 0) {
+                } else if (divisibleBy3.test(i)) {
                     result.add("Fizz");
+                } else if (divisibleBy5.test(i)) {
+                    result.add("Buzz");
                 } else {
                     result.add(String.valueOf(i));
                 }
