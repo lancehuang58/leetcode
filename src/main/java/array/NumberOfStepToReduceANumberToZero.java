23989f5d5c6f2eb5f992720eb2e402a42705ae2b
Lance Huang <lance.huang@deltaww.com>
2023-03-10 14:02:00 +0800

    number of step to reduce a number to zero.

--- src/main/java/array/NumberOfStepToReduceANumberToZero.java
+++ src/main/java/array/NumberOfStepToReduceANumberToZero.java
@@ -1,9 +1,25 @@
 package array;
 
 public class NumberOfStepToReduceANumberToZero {
+
     public static class Solution {
-        public int numberOfSteps(int num) {
-            return 0;
+
+        public int numberOfSteps(final int num) {
+            int change = num;
+            int count = 0;
+
+            while (change != 0) {
+                if (change % 2 == 0) {
+                    change /= 2;
+                    count++;
+                }
+
+                if (change % 2 != 0) {
+                    change--;
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
