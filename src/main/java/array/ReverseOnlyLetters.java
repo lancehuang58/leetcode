85053f8b7681f674a16b10ba26d5ac7760e2ac8d
refactor <lancehuang79@gmail.com>
2023-03-15 17:37:39 +0800

    reverse only letters.

--- src/main/java/array/ReverseOnlyLetters.java
+++ src/main/java/array/ReverseOnlyLetters.java
@@ -1,5 +1,29 @@
 package array;
 
 public class ReverseOnlyLetters {
+    public String reverseOnlyLetters(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
 
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            }
+
+            if (!Character.isLetter(chars[right])) {
+                right--;
+            }
+
+            if (Character.isLetter(chars[left]) && Character.isLetter(chars[right])) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+
+                left++;
+                right--;
+            }
+
+        }
+        return new String(chars);
+    }
 }
